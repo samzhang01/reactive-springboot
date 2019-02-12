7168d7854e81b15e566fa8957f3d8c127dcc9f68
Vincent Dagoury ☁ <vincent.dagoury@gmail.com>
2019-02-12 23:00:54 +0100

    The server works

--- src/main/java/org/vincedgy/reactive/ReactiveApplication.java
+++ src/main/java/org/vincedgy/reactive/ReactiveApplication.java
@@ -3,7 +3,6 @@ package org.vincedgy.reactive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +10,9 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.Disposable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-
-import java.util.function.Consumer;
 
 @SpringBootApplication
 public class ReactiveApplication {
@@ -32,21 +30,37 @@ public class ReactiveApplication {
     @EventListener(ApplicationReadyEvent.class)
     public void writeDataToMongoDB() throws Exception {
 
-        // Creating dummy data in a Flux of String
-        Flux<String> names = Flux.just("Josh", "Stephan", "Mark", "Jane", "Madhura", "Jennifer", "Vader", "Maroua");
+        this.reservationRepository
+                .deleteAll()
+                .thenMany(
+                    Flux.just("Josh", "Stephan", "Mark", "Jane", "Madhura", "Jennifer", "Vader", "Maroua")
+                        .map(name -> new Reservation(null, name))
+                        .flatMap(this.reservationRepository::save)
+                    )
+                .thenMany(this.reservationRepository.findAll())
+                .subscribe(reservation -> System.out.println("reservation : " + reservation.toString()));
 
-        // Build a collection of Reservation based on the dummy Flux of String
-        Flux<Reservation> reservations = names.map(name -> new Reservation(null, name));
-
-        // Then save the collection with the reservationRepository
-        Flux<Reservation> saved = reservations.flatMap(this.reservationRepository::save);
-
-        // Start to process with publish
-        saved.subscribe(reservation -> System.out.println("reservation : " + reservation.toString()));
 
     }
 
 }
+
+@RestController
+class ReservationRestController {
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    ReservationRestController(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    @GetMapping("/reservations")
+    Flux<Reservation> getAllReservations() {
+        return reservationRepository.findAll();
+    }
+
+}
+
 
 interface ReservationRepository extends ReactiveMongoRepository<Reservation, String> {
 }
